37e15752f5152c5a5a472735415310aaaaf8e7a0
Ruben Lopez <rubenlopsol86@gmail.com>
2023-05-31 12:49:23 +0200

    seguimos con el form del admin

--- src/components/Admin/LoginForm/LoginForm.js
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import { Button, Fom, Form } from "semantic-ui-react"
-import './LoginForm.scss'
+import React from 'react';
+import { Button, Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import './LoginForm.scss';
 
 
 
 export function LoginForm() {
+
+  const formik = useFormik({
+
+    initialValues: initialValues(),
+
+    validationSchema: Yup.object(validationSchema()),
+
+    onSubmit: (formValue) => {
+      console.log('Login ok')
+      console.log(formValue);
+    }
+
+  });
+
+
   return (
 
-    <Form className='login-form-admin'>
+    <Form className='login-form-admin' onSubmit={formik.handleSubmit} >
 
-        <Form.Input name='email' placeholder="Correo electronico" />
+      <Form.Input name='email' placeholder="Correo electronico" value={formik.values.email} onChange={formik.handleChange} error={formik.errors.email} />
 
-        <Form.Input name="password" type="password" placeholder="Contraseña" />            
-        
-        <Button type='submit' content="Iniciar sesión" primary fluid /> 
-        
+      <Form.Input name="password" type="password" placeholder="Contraseña" value={formik.values.password} onChange={formik.handleChange} error={formik.errors.password} />
+
+      <Button type='submit' content="Iniciar sesión" primary fluid />
+
     </Form>
 
   )
+}
+
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  }
+};
+
+
+function validationSchema() {
+  return {
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }
