9ecb69ac51f43792f3fe6d7a55430fc7ca67d107
Ruben Lopez <rubenlopsol86@gmail.com>
2023-06-12 11:20:55 +0200

    validacion del formulario de productos

--- src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
+++ src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { Form, Image, Button, Dropdown, Checkbox } from "semantic-ui-react"
 import { map } from "lodash";
+import { useFormik } from "formik"
+import * as Yup from "yup"
 import { useDropzone } from "react-dropzone"
 import { useCategory } from "../../../../hooks"
 import "./AddEditProductForm.scss"
@@ -23,8 +25,19 @@ export function AddEditProductForm() {
     setCategoriesFormat( formatDropdownData(categories) )
   }, [categories])
 
-  const onDrop = useCallback( (acceptedFile) => {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(newSchema()),
+    validationOnChange: false,
+    onSubmit: (formValue) => {
+      console.log('Formulario enviado');
+      console.log(formValue);
+    },
+  })
+
+  const onDrop = useCallback( async (acceptedFile) => {
     const file = acceptedFile[0];
+    await formik.setFieldValue("image", file);
     setPreviewImage( URL.createObjectURL(file));
   },[])
 
@@ -40,9 +53,25 @@ export function AddEditProductForm() {
 
   return (
 
-    <Form className='add-edit-product-form'>
-      <Form.Input name="title" placeholder="Nombre del producto" />
-      <Form.Input type="number" name="price" placeholder="Precio" />
+    <Form className='add-edit-product-form' onSubmit={ formik.handleSubmit } >
+      <Form.Input 
+      name="title" 
+      placeholder="Nombre del producto" 
+      value={formik.values.title} 
+      onChange={formik.handleChange} 
+      error={formik.errors.title} 
+
+      />
+
+      <Form.Input 
+      type="number" 
+      name="price" 
+      placeholder="Precio" 
+      value={formik.values.price} 
+      onChange={formik.handleChange} 
+      error={formik.errors.price}
+
+      />
 
       <Dropdown 
       placeholder='Categoria' 
@@ -50,17 +79,25 @@ export function AddEditProductForm() {
       selection 
       search 
       options={ categoriesFormat } 
+      value= {formik.values.category}
+      error= {formik.errors.category}
+      onChange={(_,data) => formik.setFieldValue('category', data.value)}
 
       />
 
       <div className='add-edit-product-form__active'>
-        <Checkbox toggle />
+        <Checkbox 
+        toggle 
+        checked={formik.values.active} 
+        onChange={(_,data) => formik.setFieldValue('active', data.checked) } 
+
+        />
         Producto activo
         
       </div>
 
-      <Button type='button' fluid  { ...getRootProps() } >
-        Subir imagen
+      <Button type='button' fluid  { ...getRootProps() } color={ formik.errors.image && "red" }>
+        {previewImage ? "Cambiar imagen" : "Subir imagen"}
       </Button>
       <input { ...getInputProps() } />
       <Image src={previewImage} />
@@ -80,4 +117,25 @@ function formatDropdownData(data) {
     text: item.title,
     value: item.id,
   }));
+}
+
+function initialValues(){
+  return {
+    title: "",
+    price:"",
+    category: "",
+    active: false,
+    image:"",
+
+  }
+}
+
+function newSchema(){
+  return{
+    title:Yup.string().required(true),
+    price: Yup.number().required(true),
+    category: Yup.number().required(true),
+    active: Yup.boolean().required(true),
+    image: Yup.mixed().required(true),
+  }
 }
