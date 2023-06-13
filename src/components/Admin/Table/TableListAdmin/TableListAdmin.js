6ad230fbd830551f36f51e1c20704f47d274bdbf
Ruben Lopez <rubenlopsol86@gmail.com>
2023-06-13 13:10:57 +0200

    pedidos

--- src/components/Admin/Table/TableListAdmin/TableListAdmin.js
+++ src/components/Admin/Table/TableListAdmin/TableListAdmin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Icon, Checkbox } from "semantic-ui-react"
 import { map } from "lodash"
 import { TableAdmin } from "../."
 import "./TableListAdmin.scss"
@@ -9,6 +10,18 @@ export function TableListAdmin(props) {
 
   return (
     <div className='table-list-admin'>
+
+        <Button primary icon className='table-list-admin__reload' onClick={() => console.log('onRefreshReload')} >
+            <Icon name="refresh" />
+        </Button>
+
+        <div className='table-list-admin__reload-toggle'> 
+
+            <span>Reload automatico</span>
+
+            <Checkbox toggle onChange={(_,data) => console.log(data.checked)} />
+            
+        </div>
 
         {map(tables, (table, index) => (
             <TableAdmin key={index} table={table} />
